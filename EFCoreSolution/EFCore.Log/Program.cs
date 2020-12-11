2238f8f2367efa882a44f1b25002b8f7cc29e578
LvFeng <lv.feng@outlook.com>
2020-12-11 10:28:26 +0800

    源代码

---
+++ EFCoreSolution/EFCore.Log/Program.cs
@@ -0,0 +1,20 @@
+﻿using EFCore.Log.Entity;
+using System;
+
+namespace EFCore.Log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext = new BloggingContext())
+            {
+                dbContext.Add(new Blog { Url = "www.google.com" });
+                dbContext.SaveChanges();
+
+                var blog = dbContext.Find<Blog>(1);
+                Console.WriteLine($"Id:{blog.Id},Url={blog.Url}");
+            }
+        }
+    }
+}
